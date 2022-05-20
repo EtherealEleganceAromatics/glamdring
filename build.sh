abe84f27e2906c9de9f1e47d035bfe746f220b1b
Tom Carrio <tom@carrio.dev>
2022-05-19 21:58:52 -0400

    fix: enable zoho verification page in build script

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ mkdir -p ./dist
 
 # copy assets for webpage
 echo 'Copying assets for webpage...'
-for dir in css fonts icons img js index.html credits.html manifest.webmanifest
+for dir in css fonts icons img js index.html credits.html manifest.webmanifest zoho-domain-verification.html
 do
   cp -r ./webpage/$dir ./dist
 done
