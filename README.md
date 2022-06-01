66c5ed74f5d7b072c97a9e76886bf489489c64a4
Tom Carrio <tom@carrio.dev>
2022-05-31 23:45:23 -0400

    feat: reduce image sizes of page and update social media

--- README.md
+++ README.md
@@ -1,25 +1,9 @@
-# ethereal elegance design
+# Ethereal Elegance Design
 
-the homepage for https://etherealelegance.design
+The main website for Ethereal Elegance Design, available [here](https://etherealelegance.design).
 
-## fonts
+## Fonts
 
 Titles: https://fonts.google.com/specimen/Halant?thickness=4&category=Serif&preview.text=EE&preview.text_type=custom&preview.size=165
 
-Text: https://fonts.google.com/specimen/Nobile?thickness=4&category=Sans+Serif&preview.text=ETHEREAL%20ELEGANCE&preview.text_type=custom
-
-## colors
-
-darks/blacks: #1c2516
-dark green: #3a5b33
-lights: #dad9c8
-light green: #afb675
-light blue: #dee8eb
-
-## images
-
-OPTIMIZE.
-
-## credits
-
-new section to give credit for those involved in our work
+Content: https://fonts.google.com/specimen/Nobile?thickness=4&category=Sans+Serif&preview.text=ETHEREAL%20ELEGANCE&preview.text_type=custom
--- build.sh
+++ build.sh
@@ -5,36 +5,56 @@ __BASE_DIR="$(dirname $0)"
 __ARTIFACTS_DIR="${__BASE_DIR}/dist"
 __WEBPAGE_DIR="${__BASE_DIR}/webpage"
 
-# clean artifacts directory
-echo 'Cleaning up existing artifacts'
-rm -rf "${__ARTIFACTS_DIR}"
+function clean_artifacts() {
+  # clean artifacts directory
+  echo 'Cleaning up existing artifacts'
+  rm -rf "${__ARTIFACTS_DIR}"
+}
 
-if [ "$1" = "clean" ]
-then
-  echo 'Successfully cleaned project'
-  exit 0
-fi
+function setup_artifacts() {
+  # setup artifacts directory
+  echo 'Setting up artifacts directory'
+  mkdir -p "${__ARTIFACTS_DIR}"
+}
 
-# setup artifacts directory
-echo 'Setting up artifacts directory'
-mkdir -p "${__ARTIFACTS_DIR}"
+function copy_to_artifacts() {
+  dir="$1"
+  cp -r "${__WEBPAGE_DIR}/$dir" "${__ARTIFACTS_DIR}/"
+}
 
-# copy assets for webpage
-echo 'Copying assets for webpage...'
-for dir in css fonts icons img js index.html credits.html manifest.webmanifest zoho-domain-verification.html
-do
-  cp -r "${__WEBPAGE_DIR}/$dir" "${__ARTIFACTS_DIR}"
-done
+function copy_website_srcs() {
+  # copy assets for webpage
+  echo 'Copying assets for webpage...'
+  for dir in css fonts icons img js index.html credits.html manifest.webmanifest zoho-domain-verification.html
+  do
+    copy_to_artifacts $dir
+  done
+}
 
-# minify website gallery images
-echo 'Minifying gallery photos...'
-__MINIFIED_DESIGN_PHOTOS_DIR="${__ARTIFACTS_DIR}/img/design-photos/minified"
-mkdir -p "${__MINIFIED_DESIGN_PHOTOS_DIR}"
-for img in "${__WEBPAGE_DIR}/img/design-photos/"*
-do
-  target_name="$(basename "$img")"
-  # echo $target_name
-  convert "$img" -resize 267x "${__MINIFIED_DESIGN_PHOTOS_DIR}/$target_name"
-done
+function minify_gallery_photos() {
+  # minify website gallery images
+  echo 'Minifying gallery photos...'
+  __MINIFIED_DESIGN_PHOTOS_DIR="${__ARTIFACTS_DIR}/img/design-photos/minified"
+  mkdir -p "${__MINIFIED_DESIGN_PHOTOS_DIR}"
+  for img in "${__WEBPAGE_DIR}/img/design-photos/"*
+  do
+    target_name="$(basename "$img")"
+    convert "$img" -resize 267x "${__MINIFIED_DESIGN_PHOTOS_DIR}/$target_name"
+  done
+}
 
+function main() {
+  clean_artifacts
+  if [ "$1" = "clean" ]
+  then
+    echo 'Successfully cleaned project'
+    exit 0
+  fi
+
+  setup_artifacts
+  copy_website_srcs
+  minify_gallery_photos
+}
+
+main $@
 echo 'Build completed!'
